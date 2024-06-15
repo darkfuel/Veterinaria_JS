2db7856f28c2b6ccdf29779982bfeefed4928beb
Abel Rivas Bautista <abel.rivas@Imperial.local>
2024-06-15 11:47:57 -0400

    index.js with logic

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('este es el index')
+const [operacion, nombre_animal, edad, tipo_animal, color_animal, enfermedad] = process.argv.slice(2)
+
+// console.log(operacion,nombre_animal,edad,tipo_animal,color_animal,enfermedad)
+
+if (operacion !== undefined) {
+ if (operacion.trim().toLowerCase() === 'registrar') {
+
+ } else if (operacion.trim().toLowerCase() === 'leer') {
+
+ } else {
+  console.log('Ingrese una opción válida')
+ }
+} else {
+ console.log('Error de entrada, seleccione una operación "Registrar ó Leer"')
+}
