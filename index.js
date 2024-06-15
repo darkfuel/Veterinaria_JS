afe733ce4d2d07c2dde02a14a66e0c015876609c
Abel Rivas Bautista <abel.rivas@Imperial.local>
2024-06-15 12:00:51 -0400

    operaciones.js leer operation

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const [operacion, nombre_animal, edad, tipo_animal, color_animal, enfermedad] = process.argv.slice(2)
-
+const {leer} = require('./operaciones')
 // console.log(operacion,nombre_animal,edad,tipo_animal,color_animal,enfermedad)
 
 if (operacion !== undefined) {
  if (operacion.trim().toLowerCase() === 'registrar') {
 
  } else if (operacion.trim().toLowerCase() === 'leer') {
-
+  leer()
  } else {
   console.log('Ingrese una opción válida')
  }
--- operaciones.js
+++ operaciones.js
@@ -1,1 +1,12 @@
 const fs = require('fs')
+
+const leer = () =>{
+ try {
+  const data = fs.readFileSync('citas.json')
+  console.log(data.toString())
+ } catch (error) {
+  console.log('Error de obtención de datos')
+ }
+}
+
+module.exports = {leer}
